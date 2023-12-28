987cdc4d83a9de902f58f74fccc17a90f11d0d90
usertam <heiyiutam@gmail.com>
2023-12-28 16:26:22 +0800

    flake.nix: refactor with extendModules

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
 
   outputs = { self, nixpkgs, agenix }: let
     specialArgs.lock = nixpkgs.lib.importJSON ./flake.lock;
-  in {
-    nixosConfigurations.x86_64-base = nixpkgs.lib.nixosSystem rec {
+  in rec {
+    nixosConfigurations.base.x86_64-linux = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       system = "x86_64-linux";
       modules = [
@@ -23,7 +23,7 @@
       ];
     };
 
-    nixosConfigurations.aarch64-base = nixpkgs.lib.nixosSystem rec {
+    nixosConfigurations.base.aarch64-linux = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       system = "aarch64-linux";
       modules = [
@@ -38,40 +38,20 @@
       ];
     };
 
-    nixosConfigurations.srv01 = nixpkgs.lib.nixosSystem rec {
-      inherit specialArgs;
-      system = "x86_64-linux";
+    nixosConfigurations.srv01 = nixosConfigurations.base.x86_64-linux.extendModules {
       modules = [
         { networking.hostName = "srv01"; }
         agenix.nixosModules.default
         ./secrets/catalog.nix
-        ./programs/common.nix
-        ./programs/doas.nix
-        ./programs/nix.nix
-        ./programs/zsh.nix
-        ./services/openssh.nix
-        ./services/rsyncd.nix
-        ./hosts/common.nix
-        ./hosts/azure-base.nix
       ];
     };
 
-    nixosConfigurations.srv02 = nixpkgs.lib.nixosSystem rec {
-      inherit specialArgs;
-      system = "x86_64-linux";
+    nixosConfigurations.srv02 = nixosConfigurations.base.x86_64-linux.extendModules {
       modules = [
         { networking.hostName = "srv02"; }
         agenix.nixosModules.default
         ./secrets/catalog.nix
-        ./programs/common.nix
-        ./programs/doas.nix
-        ./programs/nix.nix
-        ./programs/zsh.nix
-        ./services/openssh.nix
-        ./services/rsyncd.nix
         ./services/v2ray.nix
-        ./hosts/common.nix
-        ./hosts/azure-base.nix
       ];
     };
   };
