e1a7211284d21fdca1933413784565051f8e9e2d
usertam <heiyiutam@gmail.com>
2024-01-02 00:22:42 +0800

    services/v2ray: rework secrets with DynamicUser

--- secrets/catalog.nix
+++ secrets/catalog.nix
@@ -3,13 +3,27 @@
 {
   options.secrets = with lib; mkOption {
     type = types.attrsOf (types.submodule ({ config, ... }: {
-      options.require = mkEnableOption "including secret in configuration";
+      options.enable = mkEnableOption "including secret in configuration";
+      options.owner = mkOption {
+        type = types.str;
+        default = "root";
+        description = "Owner of secret file.";
+      };
+      options.group = mkOption {
+        type = types.str;
+        default = "root";
+        description = "Group of secret file.";
+      };
     }));
     default = {};
     description = "Attrset of secrets.";
   };
 
   config.age.secrets = with builtins; foldl' (x: y: x // y) {} (
-    map (x: lib.optionalAttrs config.secrets.${x}.require { ${x}.file = ./. + x; })
-      (attrNames config.secrets));
+    map (x: lib.optionalAttrs config.secrets.${x}.enable {
+      "${x}" = {
+        inherit (config.secrets.${x}) owner group;
+        file = ./. + "/${x}";
+      };
+    }) (attrNames config.secrets));
 }
--- services/v2ray.nix
+++ services/v2ray.nix
@@ -21,15 +21,45 @@
   networking.firewall.allowedTCPPorts = map
     (x: x.port) config.services.v2ray.config.inbounds;
 
-  secrets."v2ray".require = true;
+  secrets."v2ray".enable = true;
 
-  system.activationScripts."secrets.v2ray" = {
-    deps = [ "etc" "agenix" "agenixInstall" ];
-    text = ''
-      umask 077
-      SOURCE=$(readlink -f /etc/v2ray/config.json)
-      rm -f /etc/v2ray/config.json
-      ${pkgs.gnused}/bin/sed -f ${config.age.secrets.v2ray.path} $SOURCE > /etc/v2ray/config.json
+  # Secrets management is a lot messier when DynamicUser is involved.
+  # Add wrapper before ExecStart to make daemon wait for secrets.
+  # Can't use before/after, the user "v2ray" only exists during exec.
+  services.v2ray.package = let
+    wrapper = pkgs.writeScript "v2ray-wrapper" ''
+      #!${pkgs.stdenv.shell} -e
+      echo "Waiting for config to be decrypted..."
+      until [ -r /etc/v2ray/config.json ]; do
+        sleep 1
+      done
+      exec "$@"
     '';
+  in pkgs.runCommand "v2ray" {
+    buildInputs = [ pkgs.v2ray ];
+  } ''
+    mkdir -p $out/lib/systemd/system
+    cp -a ${pkgs.v2ray}/* $out
+    substituteInPlace $out/lib/systemd/system/v2ray.service \
+      --replace "ExecStart=" "ExecStart=${wrapper} "
+  '';
+
+  # Decrypt secrets and write to /etc/v2ray/config.json; wanted by v2ray.
+  systemd.services."secrets.v2ray" = {
+    wantedBy = [ "multi-user.target" "v2ray.service" ];
+    serviceConfig = let
+      configJSON = with builtins;
+        toFile "config.json" (toJSON config.services.v2ray.config);
+    in {
+      Type = "oneshot";
+      UMask = 0077;
+      ExecStart = pkgs.writeScript "secrets.v2ray" ''
+        #!${pkgs.stdenv.shell} -e
+        rm -f /etc/v2ray/config.json
+        ${pkgs.gnused}/bin/sed -f ${config.age.secrets.v2ray.path} \
+          ${configJSON} > /etc/v2ray/config.json
+        chown v2ray:v2ray /etc/v2ray/config.json
+      '';
+    };
   };
 }
