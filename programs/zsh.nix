b3c511ec8ed16cfd52ede1c683ba32d3602d5414
usertam <heiyiutam@gmail.com>
2024-01-06 02:55:14 +0800

    programs/zsh: enable spaceship for root

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -5,5 +5,18 @@
     enable = true;
     autosuggestions.enable = true;
     enableCompletion = true;
+    promptInit = ''
+      source ${pkgs.oh-my-zsh}/share/oh-my-zsh/lib/completion.zsh
+      source ${pkgs.oh-my-zsh}/share/oh-my-zsh/lib/key-bindings.zsh
+      source ${pkgs.spaceship-prompt}/share/zsh/themes/spaceship.zsh-theme
+      setopt correct
+    '' + ''
+      export LESS='-R'
+      local SPACESHIP_EXEC_TIME_PRECISION=0
+      local CORRECT_IGNORE='[_|.]*'
+    '' + builtins.concatStringsSep "\n" (map
+      (x: "alias ${x}='${x} --color=auto'")
+      [ "diff" "grep" "ls" ]
+    );
   };
 }
