5214242c1bd584d3415aa72d74e86fa5768c8a2f
usertam <heiyiutam@gmail.com>
2024-06-24 01:39:10 +0800

    programs/nix: enable optimise and force sandbox

--- programs/nix.nix
+++ programs/nix.nix
@@ -21,8 +21,9 @@
       };
     };
 
-    # Enable automatic garbage collection.
+    # Enable automatic garbage collection and optimise.
     gc.automatic = lib.mkDefault true;
+    optimise.automatic = lib.mkDefault true;
 
     # Everyone loves experimental features!
     settings = {
@@ -31,6 +32,19 @@
         "auto-allocate-uids"
       ] ++ lib.optional pkgs.stdenv.isLinux "cgroups";
       auto-allocate-uids = true;
+      sandbox = lib.mkDefault true;
+      extra-trusted-substituters = [
+        "https://nix-community.cachix.org"
+        "https://llama-cpp.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+        "https://context-minimals.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "llama-cpp.cachix.org-1:H75X+w83wUKTIPSO1KWy9ADUrzThyGs8P5tmAbkWhQc="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "context-minimals.cachix.org-1:pYxyH24J/A04fznRlYbTTjWrn9EsfUQvccGMjfXMdj0="
+      ];
     } // lib.optionalAttrs pkgs.stdenv.isLinux {
       use-cgroups = true;
     };
