ad720508b0948d337aa3eae4b46c7781a7a4b599
usertam <heiyiutam@gmail.com>
2024-07-04 18:38:23 +0800

    programs/nix: set substituters and trusted-public-keys directly
    because setting extra- does not always work and sometimes gets pruned

--- programs/nix.nix
+++ programs/nix.nix
@@ -33,13 +33,15 @@
       ] ++ lib.optional pkgs.stdenv.isLinux "cgroups";
       auto-allocate-uids = true;
       sandbox = lib.mkDefault true;
-      extra-trusted-substituters = [
+      substituters = [
+        "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://llama-cpp.cachix.org"
         "https://cuda-maintainers.cachix.org"
         "https://context-minimals.cachix.org"
       ];
-      extra-trusted-public-keys = [
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "llama-cpp.cachix.org-1:H75X+w83wUKTIPSO1KWy9ADUrzThyGs8P5tmAbkWhQc="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
