343d4895baf381db587764b8bc91c3ff7c384eb0
usertam <heiyiutam@gmail.com>
2024-07-10 22:24:20 +0800

    programs/nix-lnxbld: regress on nix.linux-builder.config
    error: a 'aarch64-linux' is required to build '...append-initrd-secrets.drv'

--- programs/nix-lnxbld.nix
+++ programs/nix-lnxbld.nix
@@ -4,8 +4,6 @@
   nix = {
     linux-builder.enable = true;
     linux-builder.ephemeral = true;
-    linux-builder.config.virtualisation.cores = 4;
-    linux-builder.config.nix.extraOptions = "keep-failed = true";
     settings.trusted-users = lib.mkDefault [];
   };
 }
