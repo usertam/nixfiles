8366deb73d28374a0941d85bd56ac8e6b8543d27
usertam <heiyiutam@gmail.com>
2024-10-20 19:38:02 +0800

    refactor to hosts/configs

--- flake.nix
+++ flake.nix
@@ -4,45 +4,42 @@
     systems.url = "github:usertam/nix-systems";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
     agenix.inputs.systems.follows = "systems";
     agenix.inputs.darwin.follows = "darwin";
+    agenix.inputs.home-manager.follows = "home-manager";
   };
 
   outputs = { self, nixpkgs, systems, darwin, ... }@inputs: {
-    configs.base = let
-      common = system: nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./programs/common.nix
-          ./programs/doas.nix
-          ./programs/nix.nix
-          ./programs/zsh.nix
-          ./services/openssh.nix
-          ./services/rsyncd.nix
-          ./hosts/common.nix
-        ];
-      };
-      sysBase = nixpkgs.lib.genAttrs systems.systems common;
+    # Base configuration that includes all basic configs.
+    common = let
+      sysBase = nixpkgs.lib.genAttrs systems.systems (system:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./programs/common.nix
+            ./programs/doas.nix
+            ./programs/nix.nix
+            ./programs/zsh.nix
+            ./services/openssh.nix
+            ./services/rsyncd.nix
+            ./hosts/common.nix
+          ];
+        }
+      );
       extendSysBase = base: base // {
         extendModules = args:
           extendSysBase (builtins.mapAttrs (_: v: v.extendModules args) base);
       };
     in extendSysBase sysBase;
 
-    configs.azure = self.configs.base.extendModules {
-      modules = [
-        ./hosts/azure.nix
-      ];
-    };
-
-    configs.docker = self.configs.base.extendModules {
-      modules = [
-        ./hosts/docker.nix
-      ];
-    };
+    configs = with builtins; map
+      (map (x: head (nixpkgs.lib.splitString "." x)) (attrNames (readDir ./hosts/configs)))
+      (x: common.extendModules { modules = [ ./hosts/${x} ]; });
 
     # Finally, we extend the configurations with the hostname.
     nixosConfigurations = nixpkgs.lib.mapAttrs (name: config: config.extendModules {
