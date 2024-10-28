1476675a729186d6f01905372c4f5f8863ff13bf
usertam <heiyiutam@gmail.com>
2024-10-28 18:37:54 +0800

    programs/nix-lnxbld: set nix.linux-builder.config

--- programs/nix-lnxbld.nix
+++ programs/nix-lnxbld.nix
@@ -4,6 +4,17 @@
   nix = {
     linux-builder.enable = true;
     linux-builder.ephemeral = true;
+    linux-builder.config = { ... }: {
+      imports = [
+        ../hosts/common.nix
+        ./nix-no-gc.nix
+        ./nix.nix
+        ./zsh.nix
+      ];
+      users.users.root.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRs9DrnxB9kZIe1ZQXAJrkaiW11dNvANWaxxquXX1x2"
+      ];
+    };
     settings.trusted-users = lib.mkDefault [];
   };
 }
