c67f83b988092101e8df57fff74d97d7b83e91d0
usertam <heiyiutam@gmail.com>
2024-11-09 22:59:03 +0800

    programs/nix: add back trusted-substituters

--- programs/nix.nix
+++ programs/nix.nix
@@ -55,6 +55,13 @@
       extra-sandbox-paths = lib.optionals pkgs.stdenv.isDarwin [
         "/private/etc/ssl/openssl.cnf"
       ];
+      trusted-substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://llama-cpp.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+        "https://context-minimals.cachix.org"
+      ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
