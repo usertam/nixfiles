c8a7a79c86e65b656dfd05cee43283721192af3a
usertam <heiyiutam@gmail.com>
2024-12-28 23:24:02 +0800

    hosts/darwin-runner: don't make nproc static

--- hosts/darwin-runner.nix
+++ hosts/darwin-runner.nix
@@ -4,12 +4,14 @@
   nix.linux-builder.enable = true;
   nix.linux-builder.package = lib.makeOverridable ({ modules }: (
     pkgs.runCommand "linux-builder-with-no-hvf-for-github-actions" {
-      nativeBuildInputs = [ pkgs.darwin.linux-builder ];
+      buildInputs = with pkgs; [
+        coreutils darwin.linux-builder
+      ];
     } ''
       create_builder_orig="${pkgs.darwin.linux-builder.override { inherit modules; }}/bin/create-builder"
       run_nixos_vm_orig=$(grep -o '/nix/store/[a-z0-9-]*/bin/run-nixos-vm' "$create_builder_orig")
       mkdir -p $out/bin
-      sed "s|,accel=hvf:tcg||g;s|smp 1|smp $(nproc)|g" "$run_nixos_vm_orig" > "$out/bin/run-nixos-vm"
+      sed 's|,accel=hvf:tcg||g;s|smp 1|smp "$(${pkgs.coreutils}/bin/nproc)"|g' "$run_nixos_vm_orig" > "$out/bin/run-nixos-vm"
       sed "s|$run_nixos_vm_orig|$out/bin/run-nixos-vm|g" "$create_builder_orig" > "$out/bin/create-builder"
       chmod +x "$out/bin/run-nixos-vm" "$out/bin/create-builder"
     ''
