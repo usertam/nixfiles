3896de394ca84e8ee3666c15794e780f5759318b
usertam <code@usertam.dev>
2025-06-29 16:13:57 +0800

    hosts/darwin-runner: fix to follow upstream

--- hosts/darwin-runner.nix
+++ hosts/darwin-runner.nix
@@ -2,19 +2,30 @@
 
 {
   nix.linux-builder.enable = true;
-  nix.linux-builder.package = lib.makeOverridable ({ modules }: (
-    pkgs.runCommand "linux-builder-with-no-hvf-for-github-actions" {
-      inherit (pkgs.darwin.linux-builder) passthru;
-      buildInputs = [ pkgs.darwin.linux-builder ];
-    } ''
-      createBuilder="${pkgs.darwin.linux-builder.override { inherit modules; }}/bin/create-builder"
-      runNixosVm=$(grep -o '/nix/store/[a-z0-9-]*/bin/run-nixos-vm' "$createBuilder")
+  nix.linux-builder.package = lib.makeOverridable (
+    { modules }:
+    let
+      linux-builder' = pkgs.darwin.linux-builder.override {
+        inherit modules;
+      };
+    in
+    pkgs.runCommand "linux-builder-with-no-hvf-for-github-actions"
+      {
+        inherit (linux-builder') passthru;
+        buildInputs = [ linux-builder' ];
+      }
+      ''
+        createBuilder="${linux-builder'}/bin/create-builder"
+        runBuilder=$(grep -o '/nix/store/[a-z0-9-]*/bin/run-builder' "$createBuilder")
+        runNixosVm=$(grep -o '/nix/store/[a-z0-9-]*/bin/run-nixos-vm' "$runBuilder")
 
-      mkdir -p $out/bin
-      sed 's|,accel=hvf:tcg||g' "$runNixosVm" > "$out/bin/run-nixos-vm"
-      sed "s|$runNixosVm|$out/bin/run-nixos-vm|g" "$createBuilder" > "$out/bin/create-builder"
-      chmod +x "$out/bin/run-nixos-vm" "$out/bin/create-builder"
-    '')) { modules = [ ]; };
+        mkdir -p $out/bin
+        sed 's|,accel=hvf:tcg||g' "$runNixosVm" > "$out/bin/run-nixos-vm"
+        sed "s|$runNixosVm|$out/bin/run-nixos-vm|g" "$runBuilder" > "$out/bin/run-builder"
+        sed "s|$runBuilder|$out/bin/run-builder|g" "$createBuilder" > "$out/bin/create-builder"
+        chmod +x $out/bin/*
+      ''
+  ) { modules = [ ]; };
 
   nix.linux-builder.config = {
     virtualisation.cores = 3;
@@ -22,11 +33,15 @@
   };
 
   nix.settings.trusted-users = [ "runner" ];
-  nix.settings.experimental-features = [ "nix-command" "flakes" "auto-allocate-uids" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+    "auto-allocate-uids"
+  ];
   nix.settings.auto-allocate-uids = true;
 
   # Override ugly, stupid default crap.
-  users.knownUsers = lib.mkForce [];
+  users.knownUsers = lib.mkForce [ ];
   users.knownGroups = lib.mkForce [ "nixadm" ];
 
   system.stateVersion = 5;
