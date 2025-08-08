8bceb04b477bd4d7cb0b92be097e72f53a98fb6f
usertam <code@usertam.dev>
2025-08-08 13:14:33 +0800

    hosts/tsrvbld: populate configs

--- hosts/tsrvbld.nix
+++ hosts/tsrvbld.nix
@@ -1,5 +1,50 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  # Really specific things here.
+  # We need vagrant to spawn TrustedServer mocks.
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "vagrant" ];
+  environment.systemPackages = [ pkgs.vagrant pkgs.sshpass ];
+
+  # Enable virtualbox, docker, and KVM.
+  virtualisation = {
+    docker.enable = true;
+    virtualbox.host.enable = true;
+  };
+  environment.etc."vbox/networks.conf".text = "* 0.0.0.0/0 ::/0";
+  boot.kernelModules = [ "kvm-intel" ];
+
+  # Enable x11 server and keymap.
+  services.xserver = {
+    enable = true;
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  # Enable display an desktop managers.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Enable audio.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable auto login.
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "root";
+  };
+
+  # Enable touchpad support.
+  services.libinput.enable = true;
+
+  # Don't sleep.
+  services.logind.lidSwitch = "lock";
 }
