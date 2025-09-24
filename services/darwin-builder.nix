189313fc92b48fabfa226dcfdf8e76cd83437b04
usertam <code@usertam.dev>
2025-09-24 19:01:18 +0800

    services/darwin-builder: nuke rosetta, add armv7l-linux

--- services/darwin-builder.nix
+++ services/darwin-builder.nix
@@ -5,7 +5,7 @@
     enable = true;
     ephemeral = true;
     maxJobs = 32;
-    systems = [ "aarch64-linux" "x86_64-linux" "riscv64-linux" ];
+    systems = [ "aarch64-linux" "x86_64-linux" "riscv64-linux" "armv7l-linux" ];
     config = { pkgs, ... }: {
       imports = [
         ../hosts/common.nix
@@ -22,30 +22,9 @@
           "-nic vmnet-shared,model=virtio-net-pci"
         ];
       };
-
-      boot.binfmt.emulatedSystems = [ "riscv64-linux" ];
-
-      boot.binfmt.registrations.rosetta = {
-        interpreter = "${inputs.rosetta.packages.${pkgs.system}.default}/bin/rosetta";
-
-        # The required flags for binfmt are documented by Apple:
-        # https://developer.apple.com/documentation/virtualization/running_intel_binaries_in_linux_vms_with_rosetta
-        magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
-        mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
-        fixBinary = true;
-        matchCredentials = true;
-        preserveArgvZero = false;
-
-        # Remove the shell wrapper and call the runtime directly
-        wrapInterpreterInShell = false;
-      };
-
+      boot.binfmt.emulatedSystems = [ "riscv64-linux" "armv7l-linux" "x86_64-linux" ];
       nix.settings = {
-        extra-platforms = [ "x86_64-linux" "riscv64-linux" ];
-        extra-sandbox-paths = [
-          "/run/binfmt"
-          "${inputs.rosetta.packages.${pkgs.system}.default}/bin"
-        ];
+        extra-platforms = [ "x86_64-linux" "riscv64-linux" "armv7l-linux" "x86_64-linux" ];
       };
     };
   };
