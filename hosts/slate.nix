aa390e04a151891036124ba66d14e121362d6c4a
usertam <code@usertam.dev>
2025-10-16 03:08:32 +0800

    build release image for slate

--- hosts/slate.nix
+++ hosts/slate.nix
@@ -1,11 +1,18 @@
-{ modulesPath, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
   imports = [
-    "${modulesPath}/installer/sd-card/sd-image-aarch64-new-kernel.nix"
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
   ];
 
   # Host identity.
   networking.hostName = "slate";
   system.nixos.tags = [ "slate" ];
+
+  # Use latest kernel, but disable ZFS support.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.supportedFilesystems.zfs = lib.mkForce false;
+
+  # Make release image.
+  system.build.release = config.system.build.sdImage;
 }
