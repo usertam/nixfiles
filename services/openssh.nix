5f714a41fd6fa757bb1c55a335a4a7d79ee93729
usertam <code@usertam.dev>
2025-10-26 15:40:46 +0800

    services/openssh: fix wrong use of // and lib.mkIf

--- services/openssh.nix
+++ services/openssh.nix
@@ -1,8 +1,8 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  # Abstract over setting authorized keys. We reuse the same key anyway.
-  # Warning: this will allow self-login as root on systems with the key.
+  # Abstract over setting authorized keys per user. We reuse the same key anyway.
+  # Warning: this will allow self-login as root if a regular user stores the key on the same system.
   options.users.sshUsers = lib.mkOption {
     type = with lib.types; listOf str;
     default = [ "root" ];
@@ -12,16 +12,30 @@
   config = {
     services.openssh = {
       enable = true;
+    }
+    // lib.optionalAttrs pkgs.stdenv.isLinux {
       settings = {
         PasswordAuthentication = false;
         KbdInteractiveAuthentication = false;
       };
+    }
+    // lib.optionalAttrs pkgs.stdenv.isDarwin {
+      extraConfig = ''
+        PasswordAuthentication no
+        KbdInteractiveAuthentication no
+      '';
     };
-  } // lib.mkIf (config.users.sshUsers != []) {
-    users.users = builtins.foldl' (attr: user: attr // {
-      "${user}".openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRs9DrnxB9kZIe1ZQXAJrkaiW11dNvANWaxxquXX1x2"
-      ];
-    }) {} config.users.sshUsers;
+
+    users.users = lib.mkIf (config.users.sshUsers != [ ]) (
+      builtins.foldl' (
+        attr: user:
+        attr
+        // {
+          "${user}".openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRs9DrnxB9kZIe1ZQXAJrkaiW11dNvANWaxxquXX1x2"
+          ];
+        }
+      ) { } config.users.sshUsers
+    );
   };
 }
