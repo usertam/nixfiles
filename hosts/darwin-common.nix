3d0ee54362776c115e2750c4b939d7b8e7a43dac
usertam <code@usertam.dev>
2025-11-03 19:03:23 +0800

    hosts/darwin-common: put coreutils in system sw path

--- hosts/darwin-common.nix
+++ hosts/darwin-common.nix
@@ -13,14 +13,22 @@
   ];
 
   # Override darwin-rebuild in systemPackages.
-  environment.systemPackages = lib.singleton (pkgs.runCommand "darwin-rebuild" {
-    src = [ inputs.darwin.packages.${pkgs.stdenv.hostPlatform.system}.darwin-rebuild ];
-    meta.priority = -10;
-  } ''
-    cp -a $src $out
-    chmod +w $out/bin $out/bin/darwin-rebuild
-    sed -i 's+^flake=+flake=${config.environment.darwinConfig}+' $out/bin/darwin-rebuild
-  '');
+  # Also, I want coreutils in /nix/var/nix/profiles/system/sw/bin.
+  environment.systemPackages =
+    let
+      darwin-rebuild' = pkgs.runCommand "darwin-rebuild" {
+        src = [ inputs.darwin.packages.${pkgs.stdenv.hostPlatform.system}.darwin-rebuild ];
+        meta.priority = -10;
+      } ''
+        cp -a $src $out
+        chmod +w $out/bin $out/bin/darwin-rebuild
+        sed -i 's+^flake=+flake=${config.environment.darwinConfig}+' $out/bin/darwin-rebuild
+      '';
+    in
+    [
+      darwin-rebuild'
+      pkgs.coreutils
+    ];
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
