30250c0fa915d1d53c1515e9c2b89755d0ae2a37
Samuel Tam <code@usertam.dev>
2025-12-22 19:05:28 +0800

    programs/common: add more networking tools

--- programs/common.nix
+++ programs/common.nix
@@ -2,11 +2,16 @@
 
 {
   environment.systemPackages = with pkgs; [
+    bind # dig, nslookup
     btop
+    ethtool
     file
     git
+    iperf3
+    nmap
     socat
     tmux
+    wireguard-tools
     (if stdenv.isDarwin then ghostty-bin else ghostty).terminfo
   ];
 }
