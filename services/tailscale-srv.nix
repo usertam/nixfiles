c2a514f1714d9593dcba4967acff62ba61e9cec5
usertam <code@usertam.dev>
2026-01-02 04:56:15 +0800

    services/tailscale-srv: init

---
+++ services/tailscale-srv.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable tailscale derper server.
+  services.tailscale.derper = {
+    enable = true;
+    # Honor the choice on services.tailscale.package.
+    package = config.services.tailscale.package.derper;
+    domain = "derp.usertam.dev" + " -stun=false"; # Hack to disable STUN.
+  };
+
+  # Enable coturn server, replacing tailscale's built-in one.
+  services.coturn.enable = true;
+
+  # Set memory limits for derper and coturn services.
+  systemd.services.tailscale-derper.serviceConfig = {
+    MemoryHigh = "160M";
+    MemoryMax = "192M";
+  };
+
+  systemd.services.coturn.serviceConfig = {
+    MemoryHigh = "96M";
+    MemoryMax = "128M";
+  };
+}
