06290dc79953168830f552213664ee2bf211cdab
usertam <code@usertam.dev>
2026-01-02 04:54:49 +0800

    hosts/common: bump soft file descriptors

--- hosts/common.nix
+++ hosts/common.nix
@@ -20,7 +20,18 @@
 
   # Link this repo read-only to /etc/nixos, assume image-based provisions.
   # Set environment.etc."nixos".enable = false for manual edits and switches.
+  # Similar to system.copySystemConfiguration.
   environment.etc."nixos".source = ./..;
+
+  # Raise soft file descriptors limit from 1024 to 65536. Hard limit remains same.
+  # Mostly for user; not too worried about services, as systemd sets it to hard limit already.
+  # You can check /proc/<pid>/limits to be sure.
+  systemd.settings.Manager.DefaultLimitNOFILE = "65536:524288";
+  systemd.user.extraConfig = "DefaultLimitNOFILE=65536:524288";
+  security.pam.loginLimits = [
+    { domain = "*"; type = "soft"; item = "nofile"; value = "65536"; }
+    { domain = "*"; type = "hard"; item = "nofile"; value = "524288"; }
+  ];
 
   # Set default login user root.
   services.getty.autologinUser = lib.mkDefault "root";
